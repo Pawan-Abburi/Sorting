2c93e88375186e8192e204fff768c5f65266446b
pa8065 <pa8065@CSELAB02-23.bennett.edu.in>
2019-01-28 09:41:23 +0530

    Bubble sort

--- src/lab_3/Lab_3.java
+++ src/lab_3/Lab_3.java
@@ -1,10 +1,37 @@
 package lab_3;
 
-public class Lab_3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+class Lab_3
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+    }
+ 
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        Lab_3 ob = new Lab_3();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
 }
